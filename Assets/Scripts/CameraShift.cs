563ba71990b3ded5191a46e5edb43f487682297e
Oliver <olt6@case.edu>
2021-09-27 01:05:04 -0400

    Movement Fixed and Environmental UI
    1. I made a simple script where you can interact with an object, by pressing 'E', and a UI will pop up. In my sandbox scene I made a Book object that pops up some text, but the script can be reused for any object with any UI wanted.
    2. It took a lot longer than expected, but I got all the movement fixed. The player only has a single jump, the player does not glitch into walls, and the player can no longer wall jump. However, I kept in commented code that I had implemented where the player could still wall jump, in case we want to go back to that. Also, right now, instead of going around and making ALL ground objects tagged as "ground", I made a tag called "Non-Jumpable" that I put on my objects so the player cannot jump on objects, or anything else you wouldn't want the player to jump on. Thought it would be easier that way.

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class CameraShift : MonoBehaviour
 {
     Camera mainCamera;
+    private static bool scroller;
 
     void Start()
     {
         mainCamera = GetComponent<Camera>();
+        scroller = true;
     }
 
     public void Shift()
     {
         mainCamera.orthographic = !mainCamera.orthographic;
+        scroller = !scroller;
+    }
+
+    public static bool getScroller()
+    {
+        return scroller;
     }
 }
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -22,18 +22,32 @@ public class Move : MonoBehaviour
     void Update()
     {
         Movement();
+        if(CameraShift.getScroller())
+        {
+            Recenter();
+        }
     }
 
     public void Movement()
     {
-        transform.Translate(_direction * moveSpeed * Time.deltaTime);
+        if(CameraShift.getScroller())
+        {
+            //transform.Translate(_direction.x * moveSpeed * Time.deltaTime, _direction.y * moveSpeed * Time.deltaTime, 0);
+            Vector3 movement = new Vector3(_direction.x * moveSpeed, rb.velocity.y, 0);
+            rb.velocity = movement;
+        } else
+        {
+            Vector3 movement = new Vector3(_direction.x * moveSpeed, rb.velocity.y, _direction.z * moveSpeed);
+            rb.velocity = movement;
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        if (isGrounded)
+        if (context.performed && isGrounded)
         {
-            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            Vector3 movement = jump * jumpForce;
+            rb.velocity = movement;
             isGrounded = false;
         }
     }
@@ -44,8 +58,39 @@ public class Move : MonoBehaviour
         _direction = new Vector3(_inputVector.x, 0, _inputVector.y);
     }
 
-    void OnCollisionStay()
+   /* void OnCollisionStay(Collision collision)
     {
-        isGrounded = true;
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    } */
+
+    void OnTriggerStay(Collider collider)
+    {
+        if(!collider.gameObject.CompareTag("Non-Jumpable"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnTriggerExit()
+    {
+        isGrounded = false;
+    }
+
+    public void Recenter()
+    {
+        if(transform.position.z != 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        }
     }
 }
