2a4dd1c5a9247c24e11dc795dd16e8089695ac9b
lizwat <emw119@case.edu>
2021-09-28 00:23:15 -0400

    Blended camera shift and tweaks on confining main camera

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -5,14 +5,83 @@ using UnityEngine;
 public class CameraShift : MonoBehaviour
 {
     Camera mainCamera;
+    private Matrix4x4 ortho,
+                       perspective;
+    public float fov = 60f,
+                        near = .3f,
+                        far = 1000f,
+                        orthographicSize = 50f;
+    private float aspect;
+    private MatrixBlender blender;
+    private bool orthoOn;
+    public bool YMaxEnabled = false;
+    public float YMaxValue = 0;
+    public bool YMinEnabled = false;
+    public float YMinValue = 0;
+    public bool XMaxEnabled = false;
+    public float XMaxValue = 0;
 
+    public bool XMinEnabled = false;
+    public float XMinValue = 0;
+    public Transform target;
+    public float smoothTime = 0.0f;
+    Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = target.position;
+        if (YMinEnabled && YMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, YMaxValue);
+        }
+        else if (YMinEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, YMinValue, target.position.y);
+        }
+        else if (YMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.y, target.position.y, YMaxValue);
+        }
+
+        if (XMinEnabled && XMaxEnabled)
+        {
+            targetPos.y = Mathf.Clamp(target.position.x, XMinValue, XMaxValue);
+        }
+        else if (XMinEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, XMinValue, target.position.x);
+        }
+        else if (YMaxEnabled)
+        {
+            targetPos.x = Mathf.Clamp(target.position.x, target.position.x, XMaxValue);
+        }
+
+        targetPos.z = transform.position.z;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
     void Start()
     {
         mainCamera = GetComponent<Camera>();
+        aspect = (float)Screen.width / (float)Screen.height;
+        ortho = Matrix4x4.Ortho(-orthographicSize * aspect, orthographicSize * aspect, -orthographicSize, orthographicSize, near, far);
+        perspective = Matrix4x4.Perspective(fov, aspect, near, far);
+        mainCamera.projectionMatrix = ortho;
+        orthoOn = true;
+        blender = (MatrixBlender)GetComponent(typeof(MatrixBlender));
+       
     }
 
     public void Shift()
     {
-        mainCamera.orthographic = !mainCamera.orthographic;
+        orthoOn = !orthoOn;
+        if (orthoOn)
+        {
+            blender.BlendToMatrix(ortho, 1f);
+        }
+        else
+        {
+            blender.BlendToMatrix(perspective, 1f);
+        } 
+       // mainCamera.orthographic = !mainCamera.orthographic;
     }
 }
