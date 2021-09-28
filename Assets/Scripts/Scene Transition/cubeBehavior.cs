7447dfec78ba1d25b07fe79e3c8b1eddaa9591e3
AustinMathuw <siraustin1234@gmail.com>
2021-09-27 20:48:43 -0400

    Update to timed rotations, still need to fix direction of rotations

--- Assets/Scripts/Scene Transition/cubeBehavior.cs
+++ Assets/Scripts/Scene Transition/cubeBehavior.cs
@@ -1,55 +1,47 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 
 public class cubeBehavior : MonoBehaviour
 {
-    //private InputSceneCube inputSceneCube;
-    private InputAction inputRotate; 
-    private InputAction inputPick;
-    private Vector3 direction;
-    [SerializeField] float rotationDegree = 22.0f;
+    public float rotateTime = 3.0f;
+    private bool rotating = false;
 
-    /*private void Awake() {
-        inputSceneCube = new InputSceneCube();
+    public void DoRotate(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Performed:
+                if (!rotating)
+                {
+                    Debug.Log("Rotate!");
+                    Vector2 inputVector = context.ReadValue<Vector2>();
+                    StartCoroutine(Rotate(90.0f, rotateTime, new Vector3(inputVector.x, 0, inputVector.y)));
+                }
+                break;
+        }
     }
 
-    private void OnEnable() {
-        inputRotate = inputSceneCube.SceneCube.Rotate;
-        inputRotate.Enable();
-
-        //inputSceneCube.SceneCube.Rotate.performed += DoRotate();
-        inputSceneCube.SceneCube.Rotate.Enable();
-
-        inputPick = inputSceneCube.SceneCube.Pick;
-        inputPick.Enable();
-
-        //inputSceneCube.SceneCube.Pick.performed += DoPick();
-        inputSceneCube.SceneCube.Pick.Enable();
-    }*/
-
-    public void DoRotate(InputAction.CallbackContext context) {
-        Debug.Log("Rotate" + context);
-        Vector2 inputVector = context.ReadValue<Vector2>();
-        direction = new Vector3(inputVector.x, 0, inputVector.y);
-    }
-
-    public void DoPick(InputAction.CallbackContext context) {
+    public void DoPick(InputAction.CallbackContext context)
+    {
         Debug.Log("Pick" + context);
     }
 
-    /*private void OnDisable() {
-        inputRotate.Disable();
-        inputPick.Disable();
-        inputSceneCube.SceneCube.Rotate.Disable();
-        inputSceneCube.SceneCube.Pick.Disable();
-    }*/
+    private IEnumerator Rotate(float degrees, float totalTime, Vector3 direction)
+    {
+        if (rotating)
+            yield return null;
+        rotating = true;
 
-    private void FixedUpdate() {
-        //Debug.Log("Movement Values " + inputRotate.ReadValue<Vector2>());
-        transform.Rotate(direction * Time.deltaTime * rotationDegree);
-    }
+        float rate = degrees / totalTime;
+        //Start Rotate
+        for (float i = 0.0f; Mathf.Abs(i) < Mathf.Abs(degrees); i += Time.deltaTime * rate)
+        {
+            Debug.Log("Time: " + Time.deltaTime * rate);
+            transform.Rotate(direction, Time.deltaTime * rate);
+            yield return null;
+        }
+        rotating = false;
+        Debug.Log("Done Rotating!");
+    }  
 }
