8e2072eea6f640fac52938e6142fdfae338588b5
lizwat <emw119@case.edu>
2021-10-05 22:33:05 -0400

    fixed scripts

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
+using UnityEngine;
 
 public class CameraShift : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class CameraShift : MonoBehaviour
                         far = 1000f,
                         orthographicSize = 5f;
     private float aspect;
-    private MatrixBlender blender;
+    //private MatrixBlender blender;
     private bool orthoOn;
 
 
@@ -32,15 +33,30 @@ public class CameraShift : MonoBehaviour
         perspective = Matrix4x4.Perspective(fov, aspect, near, far);
         mainCamera.projectionMatrix = ortho;
         orthoOn = true;
-        blender = (MatrixBlender)GetComponent(typeof(MatrixBlender));
+       // blender = (MatrixBlender)GetComponent(typeof(MatrixBlender));
         target = GameObject.Find("Player");
         scroller = true;
     }
-
-    public void Shift()
+    public IEnumerator MoveOverTime(Vector3 endPos, Vector3 endAngle)
     {
-        mainCamera.orthographic = !mainCamera.orthographic;
-        scroller = !scroller;
+        float timer = 0.0f;
+        float seconds = 1;
+        float percent;
+        Vector3 startPos = transform.position;
+        Vector3 startAngle = transform.eulerAngles;
+
+        while (timer <= seconds)
+        {
+            timer += Time.deltaTime;
+            percent = timer / seconds;
+            transform.position = Vector3.Lerp(startPos, endPos, percent);
+            transform.eulerAngles = Vector3.Lerp(startAngle, endAngle, percent);
+            yield return new WaitForEndOfFrame();
+
+        }
+        transform.position = endPos;
+        transform.eulerAngles = endAngle;
+
     }
 
     public void Shift(InputAction.CallbackContext context)
@@ -61,7 +77,7 @@ public class CameraShift : MonoBehaviour
            
             StartCoroutine(MoveOverTime(orthPos, orthAngle));
 
-            blender.BlendToMatrix(ortho, 1f);
+          //  blender.BlendToMatrix(ortho, 1f);
           
         }
         else {
@@ -70,7 +86,7 @@ public class CameraShift : MonoBehaviour
            
             StartCoroutine(MoveOverTime(perspPos, perspAngle));
 
-            blender.BlendToMatrix(perspective, 1f);
+          //  blender.BlendToMatrix(perspective, 1f);
         } 
     }
 
