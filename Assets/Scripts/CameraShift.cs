cd35a1f396f8603252b0459a41af5fa8a73b4dc4
AustinMathuw <siraustin1234@gmail.com>
2021-10-05 23:23:11 -0400

    Add player and camera to levels

--- Assets/Scripts/CameraShift.cs
+++ Assets/Scripts/CameraShift.cs
@@ -5,13 +5,37 @@ using UnityEngine;
 
 public class CameraShift : MonoBehaviour
 {
-    Camera mainCamera;
+    private Camera mainCamera;
+    public Vector3 rotation;
+    public Vector3 rotationOffset = new Vector3(10f, 0, 0);
+    public Vector3 targetOffset = new Vector3(0, 3f, 0);
+    private Matrix4x4 ortho,
+                       perspective;
+    public float fov = 60f,
+                        near = .3f,
+                        far = 1000f,
+                        orthographicSize = 5f;
+    private float aspect;
+    private MatrixBlender blender;
+    private bool orthoOn;
+
+
+    private GameObject target;
+
+    public float smoothTime = 3.0f;
     private static bool scroller;
     public bool showText = false;
 
     void Start()
     {
         mainCamera = GetComponent<Camera>();
+        aspect = (float)Screen.width / (float)Screen.height;
+        ortho = Matrix4x4.Ortho(-orthographicSize * aspect, orthographicSize * aspect, -orthographicSize, orthographicSize, near, far);
+        perspective = Matrix4x4.Perspective(fov, aspect, near, far);
+        mainCamera.projectionMatrix = ortho;
+        orthoOn = true;
+        blender = (MatrixBlender)GetComponent(typeof(MatrixBlender));
+        target = GameObject.Find("Player");
         scroller = true;
     }
     public IEnumerator MoveOverTime(Vector3 endPos, Vector3 endAngle)
@@ -33,27 +57,35 @@ public class CameraShift : MonoBehaviour
         }
         transform.position = endPos;
         transform.eulerAngles = endAngle;
+
     }
 
     public void Shift(InputAction.CallbackContext context)
     {
+
+
+
         Vector3 orthAngle = new Vector3(0, 0, 0);
         Vector3 perspAngle = new Vector3(45, 0, 0);
         Vector3 orthPos = new Vector3(0, 0, -10) + target.transform.position;
         Vector3 perspPos = new Vector3(0, 10, -10) + target.transform.position;
 
-        
+
         orthoOn = !orthoOn;
         scroller = !scroller;
         if (orthoOn)
         {
-           
+
             StartCoroutine(MoveOverTime(orthPos, orthAngle));
 
             blender.BlendToMatrix(ortho, 1f);
-          
+
         }
-        else {
+        else
+        {
+
+
+
             StartCoroutine(MoveOverTime(perspPos, perspAngle));
 
             blender.BlendToMatrix(perspective, 1f);
@@ -65,11 +97,13 @@ public class CameraShift : MonoBehaviour
         return scroller;
     }
 
-     void OnGUI(){
-        if (showText) {
+    void OnGUI()
+    {
+        if (showText)
+        {
             GUI.contentColor = Color.black;
             GUI.skin.label.fontSize = 20;
-            GUI.Label(new Rect(0,0,100,50), "3D Mode");
+            GUI.Label(new Rect(0, 0, 100, 50), "3D Mode");
         }
     }
 }
