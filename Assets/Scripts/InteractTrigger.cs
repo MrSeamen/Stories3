acb26361c2eaf4865809629d0234bf58a2eba3ff
AustinMathuw <siraustin1234@gmail.com>
2021-10-27 16:48:51 -0400

    More level 1 fixes

--- Assets/Scripts/InteractTrigger.cs
+++ Assets/Scripts/InteractTrigger.cs
@@ -7,14 +7,16 @@ public class InteractTrigger : MonoBehaviour
     //Could be rewritten to show/hide floating text instead of creating and deleteing;
 
     public GameObject FloatingTextPrefab;
-    public Vector3 pos;
+    public Vector3 offsetPos;
+    public Vector3 scale;
     private GameObject FloatingTextInstance;
 
     public void ShowFloatingText()
     {
         Debug.Log("Show Pick Up Text");
-        pos = transform.position;
-        FloatingTextInstance = Instantiate(FloatingTextPrefab, new Vector3(pos.x, pos.y + 0.5f * pos.y, pos.z), Quaternion.identity, transform);
+        Vector3 pos = offsetPos + transform.position;
+        FloatingTextInstance = Instantiate(FloatingTextPrefab, new Vector3(pos.x, pos.y, pos.z), Quaternion.identity, transform);
+        FloatingTextInstance.transform.localScale = scale;
     }
 
     private void OnTriggerEnter(Collider other)
