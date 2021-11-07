f845101636240b7b532a8d6c07488b0cf69987a7
Simon <simonrwangwrn@gmail.com>
2021-11-07 12:17:02 -0500

    stopgap solution for bridge

--- Assets/Scripts/moveTowardPoint.cs
+++ Assets/Scripts/moveTowardPoint.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class moveTowardPoint : MonoBehaviour
 {
     public float speed;
-    public Vector3 destination;
+    public Vector3 destination = new Vector3(0f, 3f, 0f);
 
     public void moveTowards()
-    {             
-        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+    {
+        //Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+        transform.localPosition += destination;
     }
 }
