dd51b66efd9b6a9feaf4e728da947237c7230a3e
Simon <simonrwangwrn@gmail.com>
2021-11-06 22:02:59 -0500

    object disappearance works, but rising bridge script causes crash

--- Assets/Scripts/moveTowardPoint.cs
+++ Assets/Scripts/moveTowardPoint.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class moveTowardPoint : MonoBehaviour
 {
-    public GameObject gameObject;
-    public Vector3 destination;
-    private Vector3 direction;
     public float speed;
+    private Vector3 pos1;
+    private Vector3 pos2;
+    public Vector3 posDiff = new Vector3(0f, 0f, 0f);
 
-    // Update is called once per frame
-    void Update()
+    public void move()
     {
-        direction = destination - gameObject.transform.position;
-        if (gameObject.transform.position != destination)
+        pos1 = transform.position;
+        pos2 = transform.position + posDiff;
+        while (true)
         {
-
-            gameObject.transform.position += direction * Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(pos1, pos2, Time.time * speed);
         }
     }
 }
