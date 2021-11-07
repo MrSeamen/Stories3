be4b3d5896c7fa4b12499f54de8dcb0b7dc0ac73
Simon <simonrwangwrn@gmail.com>
2021-11-07 12:05:04 -0500

    Level changes

--- Assets/Scripts/moveTowardPoint.cs
+++ Assets/Scripts/moveTowardPoint.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class moveTowardPoint : MonoBehaviour
 {
     public float speed;
-    private Vector3 pos1;
-    private Vector3 pos2;
-    public Vector3 posDiff = new Vector3(0f, 0f, 0f);
+    public Vector3 destination;
 
-    public void move()
-    {
-        pos1 = transform.position;
-        pos2 = transform.position + posDiff;
-        while (true)
-        {
-            transform.position = Vector3.Lerp(pos1, pos2, Time.time * speed);
-        }
+    public void moveTowards()
+    {             
+        transform.position = Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
     }
 }
