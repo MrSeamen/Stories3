f8ce769032f506b78ef03eeb368bc0532ba45958
lizwat <emw119@case.edu>
2021-11-09 19:47:58 -0500

    fixed platform pushing player under

---
+++ Assets/GroundDetection.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetection : MonoBehaviour
+{
+    public Transform player;
+    public bool isGrounded;
+    public Move move;
+    public MovingPlatform[] platforms;
+    private Collider col;
+    private bool isHitting = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = move.OnGround();
+        RaycastHit hit;
+
+        //  if (isGrounded)
+        // {
+        //Debug.Log("Player is grounded");
+
+        if (Physics.Raycast(player.transform.position, player.transform.TransformDirection(Vector3.up), out hit, Mathf.Abs(2)))
+        {
+            if (hit.collider.GetComponent<MovingPlatforms>() != null)
+            {
+                isHitting = true;
+                col = hit.collider;
+                Physics.IgnoreCollision(player.GetComponent<Collider>(), hit.collider, true);
+               // hit.collider.enabled = false;
+            }
+
+        }
+        else if (isHitting)
+        {
+            if (!(col == null))
+            {
+                isHitting = false;
+               // col.enabled = true;
+                Physics.IgnoreCollision(player.GetComponent<Collider>(), col, false);
+            }
+        }
+
+            
+      //  }
+        /**
+        if (!isGrounded)
+        {
+            if(col != null)
+            {
+                col.enabled = true;
+                Physics.IgnoreCollision(player.GetComponent<Collider>(), col, false);
+
+            }
+        } **/
+    }
+}
