05c147274e7974ce0825d505f79d898d937987c7
Simon <simonrwangwrn@gmail.com>
2021-11-15 16:55:20 -0500

    Art Asset Integration, Update to some scripts for environment, dialogue

--- Assets/Scripts/moveTowardPoint.cs
+++ Assets/Scripts/moveTowardPoint.cs
@@ -6,10 +6,22 @@ public class moveTowardPoint : MonoBehaviour
 {
     public float speed;
     public Vector3 destination = new Vector3(0f, 3f, 0f);
+    private Vector3 currentPosition;
+    private bool activate = false;
 
     public void moveTowards()
     {
-        //Vector3.MoveTowards(transform.position, destination, Time.deltaTime * speed);
-        transform.localPosition += destination;
+        //transform.localPosition += destination;
+        currentPosition = transform.position;
+        destination += currentPosition;
+        activate = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (activate)
+        {
+            transform.position = Vector3.Lerp(currentPosition, destination, Time.time*speed);
+        }
     }
 }
