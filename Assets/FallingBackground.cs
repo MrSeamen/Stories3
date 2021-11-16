ef0e1f2d00706dd7d818f8808dd7eeb118cfcb78
lizwat <emw119@case.edu>
2021-11-15 22:57:18 -0500

    added scrolling background

---
+++ Assets/FallingBackground.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBackground : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Material sky;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+
+        sky.mainTextureOffset = offset;
+    }
+}
