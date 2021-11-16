4a9e4e2d7b24979a7b6b878662317d815027e26c
lizwat <emw119@case.edu>
2021-11-16 15:09:59 -0500

    added player animation and transition script

--- Assets/FallingBackground.cs
+++ Assets/FallingBackground.cs
@@ -15,7 +15,7 @@ public class FallingBackground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 offset = new Vector2(0, Time.time * speed);
+        Vector2 offset = new Vector2(0, -(Time.time * speed));
 
         sky.mainTextureOffset = offset;
     }
---
+++ Assets/Scripts/Scene Transition/LoadAfterTime.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterTime : MonoBehaviour
+{
+    [SerializeField] private float delay = 10f;
+    [SerializeField] private string nextScene;
+    private float timeElapsed;
+
+    [SerializeField]
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime; 
+        if(timeElapsed > delay)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
