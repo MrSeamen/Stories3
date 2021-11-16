439e1d8c7e95fea4edb9f5dd5f9d1817f51565a6
lizwat <emw119@case.edu>
2021-11-16 15:46:42 -0500

    replaced sky placeholder

--- Assets/CloudFall.cs
+++ Assets/CloudFall.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingBackround : MonoBehaviour
+public class CloudFall : MonoBehaviour
 {
     public float speed = 0.5f;
-    public Material sky;
-    
+  //  public GameObject sky;
+
+    // Update is called once per frame
     void Update()
     {
         Vector2 offset = new Vector2(0, -(Time.time * speed));
 
-        sky.mainTextureOffset = offset;
+        this.transform.position = offset;
     }
 }
--- Assets/FallingIntro.cs
+++ Assets/FallingIntro.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FallingBackround : MonoBehaviour
+public class FallingIntro: MonoBehaviour
 {
     public float speed = 0.5f;
     public Material sky;
-    
+
     void Update()
     {
         Vector2 offset = new Vector2(0, -(Time.time * speed));
