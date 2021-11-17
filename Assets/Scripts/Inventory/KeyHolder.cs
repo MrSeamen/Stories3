67ab22a039589b336bd119d0ed3d2e19cbbb218e
AustinMathuw <siraustin1234@gmail.com>
2021-11-17 12:21:06 -0500

    Fix inventory issue

--- Assets/Scripts/Inventory/KeyHolder.cs
+++ Assets/Scripts/Inventory/KeyHolder.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class KeyHolder : MonoBehaviour
 {
     private Dictionary<Key.KeyType, Sprite> keyList;
-    private InventoryDialog inventoryDialog;
     public string previousScene;
 
     private void Awake()
@@ -15,13 +14,13 @@ public class KeyHolder : MonoBehaviour
 
         if (objs.Length > 1)
         {
+            objs[0].RefreshUI();
             Destroy(this.gameObject);
         }
 
         DontDestroyOnLoad(this.gameObject);
 
         keyList = new Dictionary<Key.KeyType, Sprite>();
-        inventoryDialog = GameObject.Find("UI/Inventory").GetComponent<InventoryDialog>();
     }
 
     public void AddKey(Key.KeyType keyType, Key key)
@@ -30,7 +29,17 @@ public class KeyHolder : MonoBehaviour
         keyList.Add(keyType, key.icon);
         Debug.Log("add to inventory");
 
+        InventoryDialog inventoryDialog = GameObject.Find("UI/Inventory").GetComponent<InventoryDialog>();
         inventoryDialog.ShowKey(key.icon);
+    }
+
+    public void RefreshUI()
+    {
+        InventoryDialog inventoryDialog = GameObject.Find("UI/Inventory").GetComponent<InventoryDialog>();
+        foreach (var item in keyList)
+        {
+            inventoryDialog.ShowKey(item.Value);
+        }
     }
 
 
@@ -40,6 +49,7 @@ public class KeyHolder : MonoBehaviour
         if(keyList.TryGetValue(keyType, out sprite))
         {
             keyList.Remove(keyType);
+            InventoryDialog inventoryDialog = GameObject.Find("UI/Inventory").GetComponent<InventoryDialog>();
             inventoryDialog.RemoveKey(sprite);
         }
     }
